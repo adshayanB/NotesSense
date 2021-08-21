57788463ff6de89ce1b9cb0621a7419f20e1ef76
Preyansh <preyanshkachhiapk@gmail.com>
2021-08-21 00:49:03 -0400

    added the pdf list template in the home page and added minor change in floating button

--- frontend/components/pdfList.js
+++ frontend/components/pdfList.js
@@ -10,18 +10,11 @@ const PdfList = ({ list }) => {
     { name: "peach", id: "5" },
     { name: "toad", id: "6" },
     { name: "bowser", id: "7" },
-    { name: "shaun", id: "8" },
-    { name: "yoshi", id: "9" },
-    { name: "mario", id: "10" },
-    { name: "luigi", id: "11" },
-    { name: "peach", id: "12" },
-    { name: "toad", id: "13" },
-    { name: "bowser", id: "14" },
   ]);
 
   const renderFiles = ({ item }) => {
     return (
-      <View style={styles.item}>
+      <View style={styles.itemContainer}>
         <Image
           style={styles.fileImage}
           source={require("../assets/favicon.png")}
@@ -33,33 +26,64 @@ const PdfList = ({ list }) => {
     );
   };
 
-  return (
-    <FlatList
-      numColumns={2}
-      keyExtractor={(item) => item.id}
-      data={people}
-      renderItem={renderFiles}
-      style={styles.container}
-    />
-  );
+  if(people.length > 0 ) {
+    return (
+      <FlatList
+        numColumns={2}
+        columnWrapperStyle={styles.row}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={renderFiles}
+        style={styles.container}
+      />
+    );
+  } else {
+    return(
+      <View style={styles.emptyContainer}>
+          <Text style={styles.emptyText}>No Notes So Far :(</Text>
+      </View>
+    )
+  }
+    
 };
 
 export default PdfList;
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 8,
+    padding: 8,
   },
-  item: {
+  emptyContainer: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emptyText: {
+    fontSize: 25,
+    color: "#9e9e9e",
+  },
+  itemContainer: {
+    flex: 0.5,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     marginHorizontal: 8,
     marginVertical: 8,
-    backgroundColor: "#b2ebf2",
+    backgroundColor: "#e0e0e0",
     height: 200,
     borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0.5,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 2,
+  },
+  row: {
+      justifyContent: "space-evenly"
   },
   fileImage: {
     width: "100%",
@@ -75,8 +99,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     width: "100%",
-    backgroundColor: "#eee",
+    backgroundColor: "#fff",
     borderBottomRightRadius: 12,
     borderBottomLeftRadius: 12,
   },
+  emptyNotes: {
+    flex: 1,
+    display: "flex",
+  }
 });
