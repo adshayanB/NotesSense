c705c5dd1b32cdb96939e8ebf601602aaead1dd3
Sathursan <ganasha02@gmail.com>
2021-08-20 23:17:57 -0400

    Fixed Typos

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ def sendNodesSpeech():
     except:
          print('Sorry.. run again...')
 
-    return {"message": "workes"}
+    return {"message": "works"}
 
 @app.route('/sendNotes/OCR', methods = ['POST'])
 def sendNotes():
@@ -72,7 +72,7 @@ def sendNotes():
     emailService = sendpdf("emailsendingpdf@gmail.com", 
                 f"{toEmail}", 
                 EMAIL_PASSWORD, 
-                f"{fileName} ", 
+                f"{fileName} PDF Notes", 
                 "Your Notes Are Attached To This Email.", 
                 f"{fileName}", 
                 pathlib.Path().resolve()) 
