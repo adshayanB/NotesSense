7bb015ddea12f25c4514e4fdd5eedc97297e923c
RishanRatna <rishanratna@hotmail.com>
2021-08-22 01:41:50 -0400

    Fix recording UI

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import FloatingButton from "../components/floatingButton";
-import PdfList from "../components/pdfList.js";
-import { Audio } from "expo-av";
-import * as FileSystem from "expo-file-system";
-import CustomPopupAlert from "../components/custom-popup-alert";
-import Toast from "react-native-toast-message";
-import CustomInputBox from "../components/custom-inputBox";
-import { LinearGradient } from "expo-linear-gradient";
-import Loading from "../assets/loading/loading.gif";
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import FloatingButton from '../components/floatingButton';
+import PdfList from '../components/pdfList.js';
+import { Audio } from 'expo-av';
+import * as FileSystem from 'expo-file-system';
+import CustomPopupAlert from '../components/custom-popup-alert';
+import Toast from 'react-native-toast-message';
+import CustomInputBox from '../components/custom-inputBox';
+import { LinearGradient } from 'expo-linear-gradient';
+import Loading from '../assets/loading/loading.gif';
+import MicRecording from '../assets/loading/mic-recording.gif';
 
 const Home = ({ navigation }) => {
   const [updateList, setUpdateList] = useState(false);
   const [recording, setRecording] = useState();
   const [openRecorder, setOpenRecorder] = useState(false);
   const [submitForm, setSubmitForm] = useState(false);
-  const [email, setEmail] = useState("");
-  const [filename, setFilename] = useState("");
+  const [email, setEmail] = useState('');
+  const [filename, setFilename] = useState('');
   const [showFetchLoading, setShowFetchLoading] = useState(false);
 
   const sendAudio = async () => {
@@ -27,14 +28,12 @@ const Home = ({ navigation }) => {
     const base64Audio = await FileSystem.readAsStringAsync(recording.getURI(), {
       encoding: FileSystem.EncodingType.Base64,
     });
-    console.log("BASE64");
-    console.log(base64Audio);
     response = await fetch(
-      "http://192.168.2.191:5000" + "/sendNotes/Speech2Text",
+      'http://192.168.2.191:5000' + '/sendNotes/Speech2Text',
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           toEmail: email,
@@ -46,22 +45,22 @@ const Home = ({ navigation }) => {
 
     json = await response.json();
 
-    if (json.message !== "Please Record again") {
+    if (json.message !== 'Please Record again') {
       let pdfLocation =
         FileSystem.documentDirectory + `${encodeURI(filename)}.pdf`;
       await FileSystem.writeAsStringAsync(pdfLocation, json.message, {
         encoding: FileSystem.EncodingType.Base64,
       });
       Toast.show({
-        text1: "Converted notes!",
-        text2: "Successfully converted audio to a PDF",
-        type: "success",
+        text1: 'Converted notes!',
+        text2: 'Successfully converted audio to a PDF',
+        type: 'success',
       });
     } else {
       Toast.show({
-        text1: "Error!",
-        text2: "Please record again :(",
-        type: "error",
+        text1: 'Error!',
+        text2: 'Please record again :(',
+        type: 'error',
       });
     }
 
@@ -71,7 +70,6 @@ const Home = ({ navigation }) => {
 
   const startRecording = async () => {
     try {
-      console.log("Requesting permissions..");
       await Audio.requestPermissionsAsync();
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: true,
@@ -81,7 +79,7 @@ const Home = ({ navigation }) => {
       //Initalize Recorder To Begin Recording
       const options = {
         android: {
-          extension: ".wav",
+          extension: '.wav',
           outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
           audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
           sampleRate: 44100,
@@ -89,7 +87,7 @@ const Home = ({ navigation }) => {
           bitRate: 128000,
         },
         ios: {
-          extension: ".wav",
+          extension: '.wav',
           audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
           sampleRate: 44100,
           numberOfChannels: 1,
@@ -105,7 +103,7 @@ const Home = ({ navigation }) => {
 
       setRecording(recorder);
     } catch (err) {
-      console.error("Failed to start recording", err);
+      console.error('Failed to start recording', err);
     }
   };
 
@@ -114,7 +112,6 @@ const Home = ({ navigation }) => {
     //setRecording(undefined);
     await recording.stopAndUnloadAsync();
     const uri = recording.getURI();
-    console.log("Recording stopped and stored at", uri);
     setOpenRecorder(false);
     setSubmitForm(true);
   };
@@ -142,21 +139,6 @@ const Home = ({ navigation }) => {
     );
   };
 
-  async function playSound() {
-    console.log("Loading Sound");
-    const { sound } = await Audio.Sound.createAsync(
-      { uri: recording.getURI() },
-      {
-        shouldPlay: true,
-      }
-    );
-    //await sound.setPositionAsync(0);
-    setSound(sound);
-
-    console.log("Playing Sound");
-    await sound.playAsync();
-  }
-
   return (
     <View style={styles.mainContainer}>
       <PdfList updateList={updateList} navigation={navigation} />
@@ -165,7 +147,10 @@ const Home = ({ navigation }) => {
           icon="mic"
           library="Feather"
           size={70}
-          onPress={() => setOpenRecorder(true)}
+          onPress={() => {
+            setOpenRecorder(true);
+            startRecording();
+          }}
           style={styles.action}
         />
         <FloatingButton
@@ -173,7 +158,7 @@ const Home = ({ navigation }) => {
           library="Feather"
           size={70}
           onPress={() =>
-            navigation.navigate("CameraScreen", { updateList, setUpdateList })
+            navigation.navigate('CameraScreen', { updateList, setUpdateList })
           }
           style={styles.action}
         />
@@ -181,19 +166,23 @@ const Home = ({ navigation }) => {
       <CustomPopupAlert
         open={openRecorder}
         title="Record Audio"
+        renderComponent={
+          <Image source={MicRecording} style={styles.micRecording} />
+        }
         buttons={[
           {
-            text: "Stop",
-            type: "outlined",
-            onPress: () => {
-              stopRecording();
+            text: 'Cancel',
+            type: 'outlined',
+            onPress: async () => {
+              await recording.stopAndUnloadAsync();
+              setOpenRecorder(false);
             },
           },
           {
-            text: "Start",
-            type: "emphasized",
+            text: 'Stop',
+            type: 'emphasized',
             onPress: () => {
-              startRecording();
+              stopRecording();
             },
           },
         ]}
@@ -205,15 +194,15 @@ const Home = ({ navigation }) => {
         renderComponent={inputForm()}
         buttons={[
           {
-            text: "Cancel",
-            type: "outlined",
+            text: 'Cancel',
+            type: 'outlined',
             onPress: () => {
               setSubmitForm(false);
             },
           },
           {
-            text: "Convert",
-            type: "emphasized",
+            text: 'Convert',
+            type: 'emphasized',
             onPress: () => {
               sendAudio();
             },
@@ -225,7 +214,7 @@ const Home = ({ navigation }) => {
           <LinearGradient
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 0 }}
-            colors={["#5b86e5", "#36d1dc"]}
+            colors={['#5b86e5', '#36d1dc']}
             style={styles.loadingPDF}
           >
             <Image source={Loading} style={styles.loading} />
@@ -239,21 +228,21 @@ const Home = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    width: "100%",
+    width: '100%',
     paddingHorizontal: 8,
   },
   mainContainer: {
     flex: 1,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "#fff",
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#fff',
   },
   actionsContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
     right: 20,
     bottom: 15,
     zIndex: 2,
@@ -262,27 +251,31 @@ const styles = StyleSheet.create({
     marginVertical: 5,
   },
   backDrop: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#000000cc",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000cc',
     zIndex: 2,
   },
   loading: {
-    position: "relative",
-    transform: [{ translateX: 20 }],
+    position: 'relative',
+    transform: [{ translateY: -40 }],
   },
   loadingPDF: {
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  micRecording: {
+    width: '95%',
+    resizeMode: 'contain',
   },
 });
 
